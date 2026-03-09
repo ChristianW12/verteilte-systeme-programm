51e1bf7cc58c0c9e346411859670a00b898ed04b
Christian <christian.wagner1204@gmail.com>
2026-03-09 11:31:32 +0100

    feat: add create task page and update routing; modify header for task management

--- frontend/frontend-app/src/app/app.routes.ts
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Login } from './pages/login/login';
+import { CreateTask } from './pages/create-task/create-task';
 
 
 export const routes: Routes = [
   { path: '', component: Home },
   {path: 'dashboard', component: Dashboard},
   {path: 'login', component: Login},
+  {path: 'create-task', component: CreateTask}
 ];
