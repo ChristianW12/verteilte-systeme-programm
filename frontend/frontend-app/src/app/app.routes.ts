ada0f5f35ebc35cd1425508974dbe84cb6ce2e81
Christian <christian.wagner1204@gmail.com>
2026-03-16 11:53:30 +0100

    add signup route to application routing

--- frontend/frontend-app/src/app/app.routes.ts
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { Login } from './pages/login/login';
 import { CreateTask } from './pages/create-task/create-task';
 import { Profile } from './pages/profile/profile';
 import { authGuard } from './auth.guard'; // Importiere den AuthGuard
+import { Signup } from './pages/signup/signup';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'dashboard', component: Dashboard, canActivate: [authGuard] }, // Geschützt
   { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
   { path: 'create-task', component: CreateTask, canActivate: [authGuard] }, // Geschützt
   { path: 'profile', component: Profile, canActivate: [authGuard] }, // Geschützt
   { path: '**', component: Home },
