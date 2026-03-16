ee07dedc01f70669ab5d6cb606bc92cb482ce0ef
Christian <christian.wagner1204@gmail.com>
2026-03-16 14:52:26 +0100

    add project and task card components to dashboard

--- frontend/frontend-app/src/app/pages/dashboard/dashboard.ts
+++ frontend/frontend-app/src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectCard } from '../../shared/components/project-card/project-card';
+import { TaskCard } from '../../shared/components/task-card/task-card';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ProjectCard, TaskCard],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
