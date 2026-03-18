cce00943a7a161943aece3a42f613a74687a5339
Christian <christian.wagner1204@gmail.com>
2026-03-18 12:51:25 +0100

    Add create-project and project components with routing updates

--- frontend/frontend-app/src/app/app.routes.ts
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -5,16 +5,19 @@ import { Login } from './pages/login/login';
 import { CreateTask } from './pages/create-task/create-task';
 import { Profile } from './pages/profile/profile';
 import { DetailedTask } from './pages/detailed-task/detailed-task';
-import { authGuard } from './auth.guard'; // Importiere den AuthGuard
+import { authGuard } from './auth.guard'; 
 import { Signup } from './pages/signup/signup';
+import { Project } from './pages/project/project';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'dashboard', component: Dashboard, canActivate: [authGuard] }, // Geschützt
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
-  { path: 'create-task', component: CreateTask, canActivate: [authGuard] }, // Geschützt
-  { path: 'profile', component: Profile, canActivate: [authGuard] }, // Geschützt
   { path: 'task', component: DetailedTask, canActivate: [authGuard] }, // Detailseite für Tasks
+  { path: 'task/create', component: CreateTask, canActivate: [authGuard] }, // Geschützt
+  { path: 'project', component: Project, canActivate: [authGuard] },
+  {path: 'project/create', component: CreateTask, canActivate: [authGuard]}, 
+  { path: 'profile', component: Profile, canActivate: [authGuard] }, // Geschützt
   { path: '**', component: Home },
 ];
